881a53ff167b1cf1c450ed63f257b037285d689c
Jennifer Gray <jgray7019@gmail.com>
2017-09-13 16:05:49 -0400

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 const gulp = require('gulp');
+const babel = require('babelify');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+const notify = require('gulp-notify');
 const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
 const concat = require('gulp-concat');
-const babel = require('gulp-babel');
 
-// a task to compile our sass
 gulp.task('styles', () => {
 	return gulp.src('./dev/styles/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
@@ -11,19 +17,33 @@ gulp.task('styles', () => {
 		.pipe(gulp.dest('./public/styles'))
 });
 
-// a task to compile our js
-gulp.task('scripts', () => {
-	return gulp.src('./dev/scripts/main.js')
-	.pipe(babel({
-		presets: ['es2015']
-	}))
-	.pipe(gulp.dest('./public/scripts'))
+gulp.task('js', () => {
+	return browserify('dev/scripts/app.js', {debug: true})
+		.transform('babelify', {
+			sourceMaps: true,
+			presets: ['es2015','react']
+		})
+		.bundle()
+		.on('error',notify.onError({
+			message: "Error: <%= error.message %>",
+			title: 'Error in JS 💀'
+		}))
+		.pipe(source('app.js'))
+		.pipe(buffer())
+		.pipe(gulp.dest('public/scripts'))
+		.pipe(reload({stream:true}));
 });
 
-// a task to watch all my other tasks
-gulp.task('watch', () => {
-	gulp.watch('./dev/styles/**/*.scss', ['styles']);
-	gulp.watch('./dev/scripts/main.js', ['scripts']);
+gulp.task('bs', () => {
+	return browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});
 });
 
-gulp.task('default', ['styles', 'scripts', 'watch']);
+gulp.task('default', ['bs','js','styles'], () => {
+	gulp.watch('dev/**/*.js',['js']);
+	gulp.watch('dev/**/*.scss',['styles']);
+	gulp.watch('./public/styles/style.css',reload);
+});
