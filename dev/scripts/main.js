624e44e5d7da2aeae275b2cdb4580397a84f83bc
Jennifer Gray <jgray7019@gmail.com>
2017-09-18 17:44:16 -0400

    added many styles and responsive design

---
+++ dev/scripts/main.js
@@ -0,0 +1,74 @@
+
+const portfolio = {};
+
+//Portfolio Initialize
+portfolio.init = function () {
+	portfolio.consoleWelcome();
+	portfolio.scroll();
+	portfolio.menu();
+};
+
+portfolio.menu = function() {
+   	$('.hamburger').on('click', function(){
+   		// toggle class overlay on the menu
+   		$('.header__nav').toggleClass('overlay');
+
+   		// toggle class show on the ul inside of the menu
+   		$('.header__nav ul').toggleClass('show');
+   	})
+};
+
+portfolio.scroll = function() {
+
+   $('.home-scroll').on('click', function(e) {
+    e.preventDefault;
+
+   	   $('html,body').animate({
+         scrollTop: $("header").offset().top},
+         'slow');
+    });
+
+   $('.about-scroll').on('click', function(e) {
+       e.preventDefault;
+
+       $('html,body').animate({
+            scrollTop: $(".aboutMe").offset().top},
+            'slow');
+   });
+
+   $('.projects-scroll').on('click', function(e) {
+       e.preventDefault;
+
+       $('html,body').animate({
+            scrollTop: $(".projects").offset().top},
+            'slow');
+   });
+
+   $('.contact-scroll').on('click', function(e) {
+       e.preventDefault;
+
+       $('html,body').animate({
+            scrollTop: $("footer").offset().top},
+            'slow');
+
+   });
+
+   $('.header__btn').on('click', function(e) {
+       e.preventDefault;
+
+       $('html,body').animate({
+            scrollTop: $(".aboutMe").offset().top},
+            'slow');
+   });
+};
+
+
+portfolio.consoleWelcome = function () {
+	console.log('Hi! Welcome to my site! Feel free to contact me at hello@jennifergray.ca');
+};
+
+
+// doc ready to run init
+$(function () {
+	portfolio.init();
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,70 @@
 const gulp = require('gulp');
-const babel = require('babelify');
-const browserify = require('browserify');
-const source = require('vinyl-source-stream');
-const buffer = require('vinyl-buffer');
-const browserSync = require('browser-sync');
-const reload = browserSync.reload;
-const notify = require('gulp-notify');
 const sass = require('gulp-sass');
-const plumber = require('gulp-plumber');
 const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const autoprefixer = require('gulp-autoprefixer');
+const plumber = require('gulp-plumber');
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+
+gulp.task('assets', () => {
+  // return gulp.src('./dev/assets/**/*')
+  //   .pipe(gulp.dest('./public/assets/'))
+  //   .pipe(reload({stream: true}));
+
+  return gulp.src('./dev/assets/**/*')
+    .pipe(imagemin([
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({optimizationLevel: 5}),
+      imagemin.svgo({
+        plugins: [
+          {removeViewBox: true},
+          {cleanupIDs: false}
+        ]
+      })
+    ]))
+    .pipe(gulp.dest('./public/assets/'))
+    .pipe(reload({stream: true}));
+});
+
+// a task to compile our sass
 
 gulp.task('styles', () => {
-	return gulp.src('./dev/styles/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(concat('style.css'))
-		.pipe(gulp.dest('./public/styles'))
+  return gulp.src('./dev/styles/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer('last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./public/styles/'))
+    .pipe(reload({stream: true}));
 });
 
-gulp.task('js', () => {
-	return browserify('dev/scripts/app.js', {debug: true})
-		.transform('babelify', {
-			sourceMaps: true,
-			presets: ['es2015','react']
-		})
-		.bundle()
-		.on('error',notify.onError({
-			message: "Error: <%= error.message %>",
-			title: 'Error in JS 💀'
+// a task to compile our js
+gulp.task('scripts', () => {
+	return gulp.src('./dev/scripts/**/*.js')
+    .pipe(plumber())
+		.pipe(babel({
+			presets: ['es2015']
 		}))
-		.pipe(source('app.js'))
-		.pipe(buffer())
-		.pipe(gulp.dest('public/scripts'))
-		.pipe(reload({stream:true}));
+		.pipe(gulp.dest('./public/scripts/'))
+    .pipe(reload({stream: true}));
 });
 
-gulp.task('bs', () => {
-	return browserSync.init({
-		server: {
-			baseDir: './'
-		}
-	});
+gulp.task('browser-sync', () => {
+  browserSync.init({
+    server: '.'  
+  });
 });
 
-gulp.task('default', ['bs','js','styles'], () => {
-	gulp.watch('dev/**/*.js',['js']);
-	gulp.watch('dev/**/*.scss',['styles']);
-	gulp.watch('./public/styles/style.css',reload);
+
+
+// a task to watch all of our other tasks
+gulp.task('watch', function() {
+  gulp.watch('./dev/scripts/**/*.js', ['scripts']);
+  gulp.watch('./dev/styles/**/*.scss', ['styles']);
+  gulp.watch('./dev/assets/**/*', ['assets']);
+  gulp.watch('*.html', reload);
 });
+
+gulp.task('default', ['browser-sync', 'assets', 'styles', 'scripts', 'watch'])
